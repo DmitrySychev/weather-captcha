f32b91f01a7a61577674d79966c80dbfdaafaecb
Dmitry Sychev <11450977+DmitrySychev@users.noreply.github.com>
2024-08-01 13:19:38 -0400

    .

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -2,7 +2,7 @@
 
 class WeatherController < ApplicationController
   def index
-    api_key = '79c8c7a115f831601ccb9b01ba0aa13f'
+    api_key = ENV['WEATHER_API_KEY']
     @challenge = Challenge.find_by(user_token: params[:user_token])
     @challenge = Challenge.create(user_token: SecureRandom.uuid) if @challenge.nil? || @challenge.create_at < 1.day.ago
     api = WeatherApi.new(api_key, @challenge.id)
